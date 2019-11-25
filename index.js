2689478a8ca21028a08f258f32358f57d76948f0
spdu <jmouru@gmail.com>
2019-11-25 12:12:22 +0200

    add procfile

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ let persons = [
 
   app.use(unknownEndpoint)
 
-  const PORT = 3001
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
